0e77d802248ffab684152964f76e0e9b5e2b45aa
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-04-14 12:03:51 -0400

    Update hw3.hs

--- HW3-Haskell1/hw3.hs
+++ HW3-Haskell1/hw3.hs
@@ -4,5 +4,3 @@ quicksort (x:xs) =
   let leftPart = quicksort [a | a <- xs, a <= x]
       rightPart = quicksort [a | a <- xs, a > x]
   in  leftPart ++ [x] ++ rightPart
-  
--- working quicksort -> learn how it worky and write a new one!!!
